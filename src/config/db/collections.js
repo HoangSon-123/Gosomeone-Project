2fe55f551efc3ed4003cea94730f6ed9ce2dfe0d
20120479 <tranlehieu65@gmail.com>
2022-12-14 17:30:59 +0700

    Thêm db.js

---
+++ src/config/db/collections.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    phone_number: {
+        type: String
+    },
+    user_id: {
+        type: String
+    }
+});
+
+const assess = new mongoose.Schema({
+    user_id: {
+        type: String,
+        require: true
+    },
+    rating: {
+        type: Number
+    },
+    review: {
+        type: String
+    }
+});
+
+const tripSchema = new mongoose.Schema({
+    condition: {
+        type: Number,
+        required: true
+    }, 
+    name: {
+        type: String,
+        required: true
+    },
+    start_day: {
+        type: Date,
+        required: true
+    },
+    end_day: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    organizer_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }, 
+    participants_id: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    price: {
+        type: Number
+    },
+    description: [{
+        type: String
+    }],
+    picture: {
+        type: String,
+        required: true
+    },
+    assess: [{
+        type: assess
+    }]
+
+});
+
+let User = mongoose.model("User", userSchema, "user");
+let Trip = mongoose.model("Trip", tripSchema, "trip");
+
+module.exports = { User, Trip };
---
+++ src/models/db.js
@@ -0,0 +1,61 @@
+require("../configs/collections");
+const mongoose = require("mongoose");
+
+const add = async (tableName, entity) => {
+    try {
+        Model = mongoose.model(tableName);
+        const object = new Model(entity);
+        const result = await object.save();
+        return result;
+    } catch (err) {
+        return err;
+    }
+};
+
+const getAll = async tableName => {
+    try {
+        Model = mongoose.model(tableName);
+        const result = await Model.find();
+        return result;
+    } catch (err) {
+        return err;
+    }
+}
+
+const getOne = async (tableName, fieldName, fieldValue) => {
+    try {
+        Model = mongoose.model(tableName);
+        var filter = { [fieldName]: fieldValue};
+        const result = await Model.findOne(filter);
+        return result;
+    } catch (err) {
+        return err;
+    }
+}
+
+const updateOne = async (tableName, fieldName, fieldValue, newEntity) => {
+    try {
+        Model = mongoose.model(tableName);
+        var filter = { [fieldName]: fieldValue};
+        const result = await Model.findOneAndUpdate(filter, newEntity);
+        return result;
+    } catch (err) {
+        return err;
+    }
+};
+
+const deleteOne = async (tableName, fieldName, fieldValue) => {
+    try {
+        Model = mongoose.model(tableName);
+        var filter = { [fieldName]: fieldValue};
+        const result = await Model.findOneAndDelete(filter);
+        return result;
+    } catch (err) {
+        return err;
+    }
+};
+
+
+module.exports = {
+    add, getAll, getOne, updateOne, deleteOne
+}
