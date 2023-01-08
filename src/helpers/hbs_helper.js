82e7b2b3b683da29f5f99337eeeeeddb593bd503
20120479 <tranlehieu65@gmail.com>
2023-01-08 18:29:41 +0700

    Sửa helpers

--- src/helpers/hbs_helper.js
+++ src/helpers/hbs_helper.js
@@ -1,5 +1,17 @@
 module.exports = {
-    dob_short: (user) => {
-        return user.dob.substring (0, 10);
-    }
+    isoDate: (dbDate) => {
+        return dbDate.substring(0, 10);
+    },
+    shortDate: (dbDate) => {
+        const yy = dbDate.substring(2, 4);
+
+        const mm = dbDate.substring(5, 7);
+        const monthName = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const mmm = monthName[Number(mm)];
+
+        const dd = dbDate.substring(8, 10);
+
+        return `${dd} ${mmm} ${yy}`
+    },
 }
